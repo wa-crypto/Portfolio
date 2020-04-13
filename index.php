3a69cc0d09702446baef55e858fbb024bf4b2faf
wa-crypto <52678709+wa-crypto@users.noreply.github.com>
2020-04-13 16:45:51 -0700

    Added all files from repl.it

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Sia Gupta Period 1]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+       <li><a href="Gupta_aboutme.html">AboutMe.html</a></li></br>
+      <li><a href="Gupta_projects.html">Projects.html</a></li></br>
+      <li><a href="Gupta_resume.html">Resume.html</a></li></br>
+      <li><a href="Gupta_contactme.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
